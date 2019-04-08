6c50169e2b215fd4859577654265f0aa721932fe
George2018 <George2018@WIN8>
2019-04-08 21:53:27 +1000

    change port to 8080

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 var express = require('express')
 var app = express()
-var port = process.env.PORT || 80
+//var port = process.env.PORT || 8080
+process.env.port = 8080
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
 })
+
 // responde with a health check status 200
 app.get('/check', (req, res) => {
     console.log('Health Check Request');
@@ -13,15 +15,6 @@ app.get('/check', (req, res) => {
     res.status(200).end();
 });
 
-// let status = 200;
-// setTimeout(() => {
-//     status = 503;
-//     console.log('Status changed to 503');
-// }, 20000);
-// app.get('/check', (req, res) => {
-//     console.log(`Health Check Request ${status}`);
-//     res.status(status).end();
-// });
 //response with meta data of the application
 app.get('/meta',(req,res) => {
   var pjson = require('./package.json');
@@ -35,6 +28,6 @@ app.get('/meta',(req,res) => {
    res.send(exjson);
 });
 
-app.listen(port, () => {
+app.listen(process.env.port, () => {
   console.log(`Api Server running on ${process.env.PORT} port, PID: ${process.pid}`);
 });
