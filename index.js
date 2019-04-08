dd63baaab53d29b8face4ba627c8c4f3f23545d8
George2018 <George2018@WIN8>
2019-04-08 22:41:00 +1000

    :10:41pm

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-
 var express = require('express')
 var app = express()
-//var port = process.env.PORT || 8080
-//process.env.port = 8080
+var port = process.env.PORT || 80
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
 })
-
 // responde with a health check status 200
 app.get('/check', (req, res) => {
     console.log('Health Check Request');
@@ -15,6 +12,15 @@ app.get('/check', (req, res) => {
     res.status(200).end();
 });
 
+// let status = 200;
+// setTimeout(() => {
+//     status = 503;
+//     console.log('Status changed to 503');
+// }, 20000);
+// app.get('/check', (req, res) => {
+//     console.log(`Health Check Request ${status}`);
+//     res.status(status).end();
+// });
 //response with meta data of the application
 app.get('/meta',(req,res) => {
   var pjson = require('./package.json');
@@ -28,6 +34,6 @@ app.get('/meta',(req,res) => {
    res.send(exjson);
 });
 
-app.listen(4747, () => {
+app.listen(port, () => {
   console.log(`Api Server running on ${process.env.PORT} port, PID: ${process.pid}`);
 });
